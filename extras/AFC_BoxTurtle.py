42d6c05f08d019248659f69f19c3b9cee835019b
Mike Nichols <mike@mikenichols.name>
2025-12-30 19:34:58 -0500

    update turtle ASCII pic

--- extras/AFC_BoxTurtle.py
+++ extras/AFC_BoxTurtle.py
@@ -32,19 +32,19 @@ class afcBoxTurtle(afcUnit):
 
         firstLeg = '<span class=warning--text>|</span><span class=error--text>_</span>'
         secondLeg = firstLeg + '<span class=warning--text>|</span>'
-        self.logo ='<span class=success--text>R  _____     ____\n'
-        self.logo+='E /      \  |  </span><span class=info--text>o</span><span class=success--text> | \n'
-        self.logo+='A |       |/ ___/ \n'
-        self.logo+='D |_________/     \n'
+        self.logo ='<span class=success--text>R  ______    🮣🭹🭹🭹🮢\n'
+        self.logo+='E /      🯔   🯗  </span><span class=info--text>o</span><span class=success--text>🯕 \n'
+        self.logo+='A |       \🯢🯑 🭻🭻🭻/ \n'
+        self.logo+='D |__________🯑     \n'
         self.logo+='Y {first}{second} {first}{second}\n'.format(first=firstLeg, second=secondLeg)
         self.logo+= '  ' + self.name + '\n'
 
-        self.logo_error ='<span class=error--text>E  _ _   _ _\n'
-        self.logo_error+='R |_|_|_|_|_|\n'
-        self.logo_error+='R |         \____\n'
-        self.logo_error+='O |              \ \n'
-        self.logo_error+='R |          |\ <span class=secondary--text>X</span> |\n'
-        self.logo_error+='! \_________/ |___|</span>\n'
+        self.logo_error ='<span class=error--text>E  _ _    _ _\n'
+        self.logo_error+='R |_|_|__|_|_|\n'
+        self.logo_error+='R |          \____\n'
+        self.logo_error+='O |       _       🯔\n'
+        self.logo_error+='R \______/  🯒    <span class=secondary--text>X</span>🯖 \n'
+        self.logo_error+='!            🮡────🮠</span>\n'
         self.logo_error+= '  ' + self.name + '\n'
 
     def system_Test(self, cur_lane, delay, assignTcmd, enable_movement):
