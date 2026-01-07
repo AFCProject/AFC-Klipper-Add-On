997a8e32aabbfd6be2dc50cefdb2d4e8e86ad6c5
jimmyjon711 <jcmadill1@gmail.com>
2026-01-06 18:36:54 -0700

    Adding nozzle_led_idx variable - Adding variable for nozzle led index - Fixed wording in macro descriptions - Added check to verify that status led and nozzle led index were unique

--- extras/AFC_Toolchanger.py
+++ extras/AFC_Toolchanger.py
@@ -166,9 +166,11 @@ class AfcToolchanger(afcUnit):
     }
     def cmd_AFC_SET_TOOLHEAD_LED(self, gcmd: GCodeCommand):
         """
-        Macro call to set print led in toolhead based on lane/tool mapping. Led config name needs to be
+        Macro call to set nozzle led in toolhead based on lane/tool mapping. Led config name needs to be
         set to AFC_extruder `led_name` variable. Status led in toolhead will not be affected if `status_led_idx`
-        is set in AFC_extruder config.
+        is set in AFC_extruder config. If `nozzle_led_idx` is set in AFC_extruder configuration then just
+        those leds will be turned on. If `nozzle_led_inx` is not provided then all leds not in defined in
+        `status_led_idx` will be turned on.
 
         `MAP` - is the mapped toolhead to set. ex(T0,T1,etc)
 
