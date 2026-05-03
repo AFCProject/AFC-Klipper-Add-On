152c2f8c7cf4e826e589704ea9982245953bf0ed
Kurt Kiefer <kekiefer@gmail.com>
2026-05-02 20:57:55 -0700

    Improve hub:direct unload behavior
    In hub:direct mode (toolchanger, single lane driving toolhead),
    inserting filament now parks just past the sensor by default. Loading
    the tool from there works normally. What doesn't work in this case is
    the unload. Since there is no hub to home back to, filament homes back
    to the load switch, which means that the lane loaded state is lost since
    that switch is no longer triggered.
    
    This change updates the unload behavior so that hub:direct lanes can be
    unloaded. It does this by doing the same homing move, but then
    scootching the filament forward until the load switch is triggered
    again.
    
    This change also addresses an inconsistency where hub:direct lanes can't
    be ejected. The sequencing of this now works normally with the working
    unload routine.
    
    Fixes #719

--- extras/AFC.py
+++ extras/AFC.py
@@ -1204,8 +1204,7 @@ class afc:
 
         # TODO: add a check for multi-tools to verify lane is not loaded to toolhead before trying to unload
         if (cur_lane.name != cur_lane.extruder_obj.lane_loaded
-		    and not cur_lane.extruder_obj.is_standalone()
-			and not cur_lane.is_direct_hub()):
+		    and not cur_lane.extruder_obj.is_standalone()):
             # Setting status as ejecting so if filament is removed and de-activates the prep sensor while
             # extruder motors are still running it does not trigger infinite spool or pause logic
             # once user removes filament lanes status will go to None
@@ -1232,9 +1231,6 @@ class afc:
 
         elif cur_lane.name == cur_lane.extruder_obj.lane_loaded:
             self.logger.info("LANE {} is loaded in toolhead, can't unload.".format(cur_lane.name))
-
-        elif cur_lane.is_direct_hub():
-            self.logger.info("LANE {} is a direct lane must be tool unloaded.".format(cur_lane.name))
 
         self.current_state = State.IDLE
 
@@ -2029,10 +2025,12 @@ class afc:
 
             if (cur_lane.is_direct_hub()
                 and not cur_lane.extruder_obj.is_standalone()):
-                while cur_lane.raw_load_state:
-                    cur_lane.move_advanced(cur_lane.short_move_dis * -1, SpeedMode.SHORT,
-                                           assist_active=AssistActive.YES)
-                cur_lane.move_advanced(cur_lane.short_move_dis * -5, SpeedMode.SHORT)
+                park_tries = 0
+                while not cur_lane.raw_load_state and cur_lane.prep_state:
+                    park_tries += 1
+                    cur_lane.move_advanced(cur_lane.short_move_dis, SpeedMode.SHORT)
+                    if park_tries >= 5:
+                        break
 
             if self.post_unload_macro is not None:
                 self.gcode.run_script_from_command(self.post_unload_macro)
